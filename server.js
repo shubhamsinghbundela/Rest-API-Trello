2af21a11a92cea4fe9b6e7648f264fb499d04b62
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-04-16 02:57:16 +0530

    server setup and db connected 16-04-2026

---
+++ server.js
@@ -0,0 +1,17 @@
+import "dotenv/config"
+import app from "./src/app.js"
+import connectDB from "./src/common/config/db.js"
+
+const PORT = process.env.PORT || 3000
+
+const start = async()=>{
+    await connectDB();
+    app.listen(PORT, () => {
+        console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV} mode`)
+    })
+}
+
+start().catch((err)=>{
+    console.error("Failed to start server", err);
+    process.exit(1)
+})
